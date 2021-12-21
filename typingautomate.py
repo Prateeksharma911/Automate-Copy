a444b0e8f826a1391a1226f62d69f7b8c80faa60
Prateeksharma911 <prateek1999sharma@gmail.com>
2021-12-21 22:03:56 +0530

    Fixed tab text issue

--- typingautomate.py
+++ typingautomate.py
@@ -1,16 +1,20 @@
 print("Started")
-
 import pyautogui
 import time
 
-time.sleep(4)
+time.sleep(3)
+print("Writing starts")
+time.sleep(1)
 
 # with open("D:\\Study material\\NIIT\\Pythonfile\\Linklist\\typeautomate.txt", 'r') as file:
 with open("typeautomate.txt", 'r') as file:
     text = file.read()
 
-pyautogui.write(text)
+
+text = text.replace('\t', '               ')
+
+pyautogui.typewrite(text)
 # for i in range(len(text)):
 # 	pyautogui.typewrite(text[i])
 print(text)
-input("----------Copy Done----------")
+print("----------Copy Done----------")
